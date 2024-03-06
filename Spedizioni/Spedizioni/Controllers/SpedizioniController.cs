336c8ac7b7f3a33f62770ada438bd63feeca1c0b
Francesco <napo.fra96@gmail.com>
2024-03-06 23:19:12 +0100

    css

--- Spedizioni/Spedizioni/Controllers/SpedizioniController.cs
+++ Spedizioni/Spedizioni/Controllers/SpedizioniController.cs
@@ -130,6 +130,47 @@ namespace Spedizioni.Controllers
             return View(spedizioni);
         }
 
+        public ActionResult VisualizzaSpedizioni()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Spedizioni"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+            List<Spedizioni.Models.Spedizioni> spedizioni = new List<Spedizioni.Models.Spedizioni>();
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Spedizioni";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Spedizioni.Models.Spedizioni s = new Spedizioni.Models.Spedizioni();
+                    s.IdSpedizione = Convert.ToInt32(dr["IdSpedizione"]);
+                    s.IdCliente = Convert.ToInt32(dr["FK_IdCliente"]);
+                    s.codTracciamento = dr["codTracciamento"].ToString();
+                    s.dataSpedizione = Convert.ToDateTime(dr["dataSpedizione"]);
+                    s.pesoSpedizione = Convert.ToDecimal(dr["pesoSpedizione"]);
+                    s.cittaDestinazione = dr["cittaDestinazione"].ToString();
+                    s.indirizzoDestinazione = dr["indirizzoDestinazione"].ToString();
+                    s.nominativoDestinatario = dr["nominativoDestinatario"].ToString();
+                    s.costoSpedizione = Convert.ToDecimal(dr["costoSpedizione"]);
+                    s.dataConsegna = Convert.ToDateTime(dr["dataConsegna"]);
+                    s.IdStatoSpedizione = Convert.ToInt32(dr["FK_IdStato"]);
+                    spedizioni.Add(s);
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return View(spedizioni);
+        }
+
+
         public ActionResult InserisciSpedizione()
         {
             return View();
